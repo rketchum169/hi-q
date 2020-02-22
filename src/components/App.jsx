44c59c2e2da22e9b53fd7365f11f7573c1c03581
Ryan Ketchum <ryan@Ryans-MacBook-Pro-2.local>
2020-02-22 10:37:30 -0800

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/css/app.css';
+
+function App() {
+  return (
+    <div className="App">
+      WELCOME TO WEEK L15 LAB - LET'S RUN IT
+    </div>
+  );
+}
+
+export default App;
